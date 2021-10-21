c46f20d5301b282ad6303a09f59ab5006e6f02e1
MattewMakar <mattewmakar@gmail.com>
2021-10-21 18:36:50 -0400

    Fix: sorting issue from the backend

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -3,20 +3,14 @@ import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
-/**
- * To sort Message by date
- * @param {Message} a
- * @param {Message} b
- * @returns {number}
- */
-const sortingMessagesByDate = (a, b) => moment(a.createdAt).valueOf() - moment(b.createdAt).valueOf();
+
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
   // sorting the messages by it's Epoch time milliseconds
   return (
     <Box>
-      {messages.sort(sortingMessagesByDate).map((message) => {
+      {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
         return message.senderId === userId ? <SenderBubble key={message.id} text={message.text} time={time} /> : <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />;
       })}
