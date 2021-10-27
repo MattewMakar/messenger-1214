afe6d1dac4f75b4fe3b1a786d02657b6dd3d7255
MattewMakar <mattewmakar@gmail.com>
2021-10-26 21:44:30 -0400

    Draft database redesign

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,12 +1,13 @@
 const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
+const Read = require("./read");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+Conversation.belongsToMany(User, { through: "convo_users" }); // will create a bridge table between conversations and users
+Message.belongsToMany(User, { through: Read });  // will use the read table as a bridge table between Message and users
+
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
